f51bbd58f667246c6776c437a6474cbef1521600
Filipe <89738919+fpfrances@users.noreply.github.com>
2025-04-13 15:11:23 -0400

    flask requests updated

--- backend/main.py
+++ backend/main.py
@@ -86,4 +86,4 @@ def analyze_resume():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
