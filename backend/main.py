cf7727cf8d2d57c0a56303d4221dd751bc1ae989
Filipe <89738919+fpfrances@users.noreply.github.com>
2025-04-13 15:51:47 -0400

    app run port changes

--- backend/main.py
+++ backend/main.py
@@ -91,5 +91,5 @@ def analyze_resume():
 
 # Run the app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not set by Render
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=10000, debug=True)
